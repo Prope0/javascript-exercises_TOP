d404e03db088a24f3b904bbca23cf54ffa64378d
Prope0~ <tamas.zsigo.92@gmail.com>
2023-02-19 11:56:00 +0100

    first for loop version

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,17 @@
 const reverseString = function(input) {
-    reversed = input.split("").reverse().join("");
-    return reversed
+    let result = "";
+    for (let i = input.length - 1; i >= 0; i--) {
+        result += input[i]
+    }
+return result
 };
+
+/*
+input variable in teh func parameters 
+variable where store result
+for loop for iterate through the input and this iterates save in the result
+return the result
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
@@ -13,7 +23,11 @@ module.exports = reverseString;
  do the reversal and then join it back together into a string. */
 
  /*
-
+FIRST WORKING SOLUTION FIND ON THE INTERNET:
+const reverseString = function(input) {
+    reversed = input.split("").reverse().join("");
+    return reversed
+};
 
 
 
