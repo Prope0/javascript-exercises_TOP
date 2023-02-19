65535e81b596629528ea8c60d6edf9c8d39c7b79
Prope0~ <tamas.zsigo.92@gmail.com>
2023-02-19 23:39:01 +0100

    3/3 test passed for convertToCelsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inF) {
+let toC = (inF - 32) * (5/9);
+return Math.round(toC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inC) {
+  let toF = inC * 9/5 +32;
+  return Math.round(toF * 10) / 10
 };
 
 // Do not edit below this line
