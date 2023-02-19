8df6d7122ae24ceb16759c2f02863ff1fe2a8053
Prope0~ <tamas.zsigo.92@gmail.com>
2023-02-19 09:08:42 +0100

    first non work version

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string ; numOfTimes) {
+    string = prompt('Please neter the string You want repeated: ');
+    numOfTimes = prompt('please enter the number of times You want to repeat it: ');
+    result = '' ;
+    for (let i = 0; i < numOfTimes ; i++ ) {
+        result += string
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
