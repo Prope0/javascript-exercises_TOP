6197dfa61db14c6476138886f73c8a345ae0bc48
Prope0~ <tamas.zsigo.92@gmail.com>
2023-02-19 16:03:38 +0100

    6/6 test passed solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,17 @@
-const sumAll = function (min, max)/*(4, 1)*/ {
-    if (min > max) {
-        let temp = min;
-        min = max;
-        max = temp;
-    }
-    let sum = 0;
-    for (let i = min ; i <= max ; i++) {
-        sum += i
-    }
-  return sum
-  };
+const sumAll = function (min, max) /*(4, 1)*/ {
+  if ( typeof min != 'number' || typeof max != 'number') {return "ERROR" }
+  if (min < 0 || max < 0) { return "ERROR" }
+  if (min > max) {
+    let temp = min;
+    min = max;
+    max = temp;
+  }
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+};
 
 /*
 sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
